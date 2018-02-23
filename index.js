390a3fd6f1241b36308ccbfb018bfda691099347
Vasil Slavov <v.slavov96@gmail.com>
2018-02-23 14:18:20 +0200

    Add helper for contains, refactor old helpers

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const loadHelpers = handlebars => {
-  handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
-    if (arguments.length < 3)
-      throw new Error('Handlebars Helper equal needs 2 parameters');
-    if (lvalue != rvalue) {
-      return options.inverse(this);
-    } else {
-      return options.fn(this);
+const helpers = [
+  {
+    name: 'equal',
+    executable: (lvalue, rvalue, options) => {
+      if (arguments.length < 3)
+	throw new Error('Handlebars Helper equal needs 2 parameters');
+      if (lvalue != rvalue) {
+	return options.inverse(this);
+      } else {
+	return options.fn(this);
+      }
     }
-  });
+  },
+  {
+    name: 'containsvalue',
+    executable: (collection = [], value) => {
+      return collection.filter(item => value === item.value).length > 0;
+    }
+  }
+];
+
+
+const loadHelpers = handlebars => {
+  helpers.map(item => handlebars.registerHelper(item.name, item.executable));
   return handlebars;
 };
 
